fff6bf36e3e223873f1b2499d02771f65ac765d0
salahdine2020 <67063037+salahdine2020@users.noreply.github.com>
2025-01-13 16:43:08 +0100

    Delete lib directory

--- lib/core/constants/colors.dart
+++
@@ -1,8 +0,0 @@
-// core/constants/colors.dart
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primary = Color(0xFF0A0A0A);
-  static const Color secondary = Color(0xFF3A3A3A);
-  static const Color accent = Color(0xFFE91E63);
-}
